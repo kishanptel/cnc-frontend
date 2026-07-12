2a0e772df1c40173634a372a4075ce1cc7df1090
Kishan <kishanptel0709@gmail.com>
2026-07-13 00:39:46 +0530

    cors

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "https://sweetshop-backend-nine.vercel.app",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
