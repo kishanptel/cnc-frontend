d900690965655623b9b1380eff459f4fbf66ff69
Kishan <kishanptel0709@gmail.com>
2026-07-13 04:35:18 +0530

    cors error

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 
+let apiURL = import.meta.env.VITE_API_URL;
+if (apiURL && !apiURL.startsWith('http://') && !apiURL.startsWith('https://')) {
+    apiURL = `https://${apiURL}`;
+}
+
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: apiURL,
     withCredentials: true
 })
 
